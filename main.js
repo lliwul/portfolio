ad4a53de492003d0c8808a573e8ed4f9bb7435ec
Lu Williams <luwilliams@Lus-MacBook-Air.local>
2026-05-20 10:32:09 -0500

    latest push

--- main.js
+++ main.js
@@ -3,53 +3,86 @@ let msg = "Hi Lu Js is working"
 console.log(msg)
 
 
+function hideAllProjects() {
+    // Grab every element with the project class
+    const allProjects = document.querySelectorAll('.hiddenWindow');
+    allProjects.forEach(project => {
+        project.style.display = 'none';
+    });
+}
+
 const linkedInBtn = document.getElementsByClassName('linkedinBranding')[0]
 
 
+
 linkedInBtn.addEventListener('click', function(event){
+    console.log('LinkedIn Button was clicked!');
 
-        let clickCount = 0;
-        clickCount ++;
-        console.log(clickCount)
+    hideAllProjects();
 
-        console.log('Button was clicked!');
+    const hiddenWindow = document.getElementsByClassName('linkedInProject')[0]
 
-       
+    const hideWindow = document.getElementsByClassName('folioProfile')[0]
+
+
+    hiddenWindow.style.display = 'block';
+
+    hideWindow.style.display = 'none';
 
         
 });
 
 const emailBtn = document.getElementsByClassName('emailDesign')[0]
-emailBtn.onclick = function() {
-    console.log('Button clicked!');
+    emailBtn.onclick = function() {
+    console.log('Email Button was clicked!');
 
-    const hiddenWindow = document.getElementsByClassName('folioExamples')[0]
+    hideAllProjects();
+
+    const hiddenWindow = document.getElementsByClassName('emailProject')[0]
+
+    const hideWindow = document.getElementsByClassName('folioProfile')[0]
+
+    hiddenWindow.style.display = 'block';
+    hideWindow.style.display = 'none';
+  
+};
+
+const brightrBtn = document.getElementsByClassName('brightergreen')[0]
+
+
+brightrBtn.onclick = function() {
+    console.log('Brightr Button clicked!');
+
+    hideAllProjects();
+
+    const hiddenWindow = document.getElementsByClassName('brighterGreenProject')[0]
 
     const hideWindow = document.getElementsByClassName('folioProfile')[0]
 
     hiddenWindow.style.display = 'block';
 
     hideWindow.style.display = 'none';
-};
 
-const brightrBtn = document.getElementsByClassName('brightergreen')[0]
-
-brightrBtn.onclick = function() {
-    console.log('Button clicked!');
 };
 
 
-const exitBtn = document.getElementsByClassName('exitButton')[0]
+const exitButtons = document.querySelectorAll('.exitButton');
 
-    exitBtn.onclick = function() {
-    console.log('Button clicked!');
+    exitButtons.forEach(button => {
+    // 3. Attach the click event to EVERY button
+    button.onclick = function() {
+        console.log('Exit Button clicked!');
 
+        // Hide all elements with 'hiddenWindow'
+        const hiddenWindows = document.getElementsByClassName('hiddenWindow');
+        for (let i = 0; i < hiddenWindows.length; i++) {
+            hiddenWindows[i].style.display = 'none';
+        }
 
-    const hiddenWindow = document.getElementsByClassName('folioExamples')[0]
-
-    const hideWindow = document.getElementsByClassName('folioProfile')[0]
-
-    hiddenWindow.style.display = 'none';
-
-    hideWindow.style.display = 'block';
-};
+        // Show the 'folioProfile' window
+        const hideWindow = document.getElementsByClassName('folioProfile')[0];
+        if (hideWindow) {
+            hideWindow.style.display = 'block';
+        }
+    };
+});
